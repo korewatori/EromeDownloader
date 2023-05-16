c6d3a03e3d8a182484bbb935a9809d96f23db629
Tori <76889884+korewatori@users.noreply.github.com>
2023-05-16 00:50:28 +0000

    Update dump.py to add various changes
    In this fork of EromeDownloader, I have changed around a few things and added other bits.
    
    Changes:
    - Implemented tqdm for a simple progress bar for each download
    - Now prints basic information about the album, such as the title and number of files
    - ANSI colour-coded to help stand out more
    - Added basic "Files Complete/Files Skipped/Files Failed" that is displayed at the end of the album download process.
    
    Known issues:
    - "Files Complete" doesn't list the amount of completed files (at least, this happened during testing). 
    This, however, works as intended with "Files Skipped".

--- dump.py
+++ dump.py
@@ -1,13 +1,18 @@
 import argparse
 import sys
+import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-import os
 import tldextract
+from tqdm import tqdm
+
+COLOR_RED = "\x1b[38;5;1m"
+COLOR_GREEN = "\x1b[32m"
+COLOR_YELLOW = "\x1b[38;5;3m"
+COLOR_END = "\033[0m"
 
 session = requests.Session()
-
 
 def collect_links(album_url):
     parsed_url = urlparse(album_url)
@@ -27,8 +32,38 @@ def collect_links(album_url):
     urls = list(set([*videos, *images]))
     download_path = get_final_path(title)
     existing_files = get_files_in_dir(download_path)
+
+    print(f"Downloading files from album: {album_url}")
+    print(f"Title: {title}")
+    print(f"Total Files: {len(urls)}")
+    # Uncomment the lines below if you want the list of files to be printed to the console.
+    # print("Files to be downloaded:")
+    # for file_url in urls:
+    #    print(file_url + COLOR_END)
+    print("\nStarting Download...\n")
+
+    files_complete = 0
+    files_skipped = 0
+    files_failed = 0
+
     for file_url in urls:
-        download(file_url, download_path, album_url, existing_files)
+        result = download(file_url, download_path, album_url, existing_files)
+        if result == "downloaded":
+            files_complete += 1
+        elif result == "skipped":
+            files_skipped += 1
+        elif result == "failed":
+            files_failed += 1
+
+    clear_console()
+    print(
+    f"{COLOR_GREEN}| Files Complete: {files_complete} {COLOR_YELLOW}- Files Skipped: {files_skipped} {COLOR_RED}- Files Failed: {files_failed} {COLOR_END}|\n"
+    )
+    print(f'Finished Downloading {album_url}. Enjoy :)')
+
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def get_final_path(title):
@@ -48,9 +83,9 @@ def download(url, download_path, album=None, existing_files=[]):
     parsed_url = urlparse(url)
     file_name = os.path.basename(parsed_url.path)
     if file_name in existing_files:
-        print(f'[#] Skipping "{url}" [already downloaded]')
-        return
-    print(f'[+] Downloading "{url}"')
+        print(COLOR_YELLOW + f'[#] Skipping "{url}" [already downloaded]' + COLOR_END)
+        return "skipped"
+
     extracted = tldextract.extract(url)
     hostname = "{}.{}".format(extracted.domain, extracted.suffix)
     with session.get(
@@ -58,19 +93,25 @@ def download(url, download_path, album=None, existing_files=[]):
         headers={
             "Referer": f"https://{hostname}" if album is None else album,
             "Origin": f"https://{hostname}",
-            "User-Agent": "Mozila/5.0",
+            "User-Agent": "Mozilla/5.0",
         },
         stream=True,
     ) as r:
         if r.ok:
-            with open(os.path.join(download_path, file_name), "wb") as f:
+            file_path = os.path.join(download_path, file_name)
+            total_size = int(r.headers.get("content-length", 0))
+            progress = tqdm(total=total_size, unit="B", unit_scale=True)
+            with open(file_path, "wb") as f:
                 for chunk in r.iter_content(chunk_size=1024):
-                    f.write(chunk)
+                    if chunk:
+                        f.write(chunk)
+                        progress.update(len(chunk))
+            progress.close()
+            print(f'[#] Downloaded "{url}"' + COLOR_END)
         else:
-            print(r)
-            print(f'[ERROR] Download of  "{url}" failed')
+            print(COLOR_RED + str(r))
+            print(COLOR_RED + f'[-] Failed to Download "{url}"' + COLOR_END)
             return None
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(sys.argv[1:])
